1cfea0efd581332eab38be6f34d7aabd3fc0d8c3
phonglan123 <32542949+phonglan123@users.noreply.github.com>
2021-06-07 21:44:38 +0700

    Update v1.5

--- MiniProjects/pefuha.js
+++ MiniProjects/pefuha.js
@@ -1,11 +1,11 @@
+<script>
 class Pefuha {
     // pefuha = PetaFunHash [^^]
-    constructor(version = 1) {
-        this.version = version;
-    }
+    constructor() {}
     hash(string, hashLength = 4, randomEightNum = Math.floor(10000000 + Math.random() * 90000000), randomOneNum = Math.floor(Math.random() * 9) + 1) {
         var string = string.split(''),
-            seed = randomEightNum.toString() + randomOneNum.toString() + hashLength.toString();
+            seed = randomEightNum.toString() + randomOneNum.toString() + hashLength.toString(),
+            charset = 'abcdefghijklmnoqrstuvwxyzABCDEFGHIJKLMNOQRSTUVWXYZ';
 
         for (var i = 0; i < string.length; i++) {
             var temp = Math.pow((randomEightNum * Math.pow(string[i].charCodeAt(0), randomOneNum)), 1 / 4),
@@ -13,16 +13,21 @@ class Pefuha {
             string[i] = result;
             randomEightNum += randomOneNum;
         }
+        
+        string = string.join('').match(/.{1,2}/g);
+        for (var i = 0; i < string.length; i++)
+            string[i] = charset[(string[i] - 50 < 0) ? string[i] - 0 : string[i] - 50];
 
-        return seed + '[P' + this.version + ']' + string.join('');
+        return seed + string.join('');
     }
     check(hashed, checkString) {
-        var seed = hashed.split('[P' + this.version + ']')[0],
-            checked = this.hash(checkString, parseInt(seed.substring(9)), parseInt(seed.substring(0, 8)), parseInt(seed.substring(8, 9)));
+        var checked = this.hash(checkString, parseInt(hashed.substring(9)), parseInt(hashed.substring(0, 8)), parseInt(hashed.substring(8, 9)));
         return hashed == checked;
     }
 }
 
-var pefuha = new Pefuha();
-console.log(pefuha.hash('xin chào'))
-console.log(pefuha.check(pefuha.hash('chào'), 'cháo'))
+var pefuha = new Pefuha(),
+	hashed = pefuha.hash('chào');
+console.log(hashed)
+console.log(pefuha.check(hashed, 'cháo'))
+</script>
